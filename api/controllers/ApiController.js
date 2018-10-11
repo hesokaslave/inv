91977f14531d8f9ae1458b3eb87120019435e23d
Your Name <you@example.com>
2018-10-11 19:28:18 +0100

    first commit

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -11,20 +11,31 @@ module.exports = {
         let products = await Product.find();
         let fournisseurs = await Fournisseur.find();
         let clients = await Client.find();
-
         return res.json({products,fournisseurs,clients})
     },
 
-    manuelLivraison : (req,res) => {
+    manuelLivraison : async (req,res) => {
       const { actor, product } = req.allParams()
-      sails.sockets.blast('newOp',{operation : 'Livraison',type : 'Client', actor,product})
-      return res.ok({ok : 'ok'});
+      let prods = await Product.find({ code : product});
+      let clients = await Client.find({ nom : actor });
+      Livraison.create({ quantite : 1, product : prods[0].id, client : clients[0].id }).fetch().then( async (liv) => {
+        await Product.addToCollection(prods[0].id, 'livraisons').members([liv.id]);
+        await Client.addToCollection(clients[0].id, 'livraisons').members([liv.id]);
+        sails.sockets.blast('newOp',{operation : 'Livraison',type : 'Client', actor,product})
+        return res.ok({ok : 'ok'});
+      })
     },
 
-    manuelReception : (req,res) => {
+    manuelReception : async (req,res) => {
       const { actor, product } = req.allParams()
-      sails.sockets.blast('newOp',{operation : 'Réception',type: 'Fournisseur', actor,product})
-      return res.ok({ok : 'ok'});
+      let prods = await Product.find({ code : product});
+      let fours = await Fournisseur.find({ nom : actor });
+      Reception.create({ quantite : 1, product : prods[0].id, fournisseur : fours[0].id }).fetch().then( async (liv) => {
+        await Product.addToCollection(prods[0].id, 'receptions').members([liv.id]);
+        await Fournisseur.addToCollection(fours[0].id, 'receptions').members([liv.id]);
+        sails.sockets.blast('newOp',{operation : 'Réception',type: 'Fournisseur', actor,product})
+        return res.ok({ok : 'ok'});
+      })
     }
 
 };
