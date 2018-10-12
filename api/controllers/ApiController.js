0958d2daf48042add7a78598e0eb0df92aac7fc3
Your Name <you@example.com>
2018-10-12 17:43:21 +0100

    f

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -51,13 +51,13 @@ module.exports = {
       let prods = await Product.find({ codeABarres : barCode});
       let clients = await Client.find({ nom : actor });
 
-      if( prods.length === 0 ) return res.ok({ 'error' : ` Produit Introuvable ! (${product}) `})
+      if( prods.length === 0 ) return res.ok({ 'error' : ` Produit Introuvable ! (${barCode}) `})
       if( clients.length === 0 ) return res.ok({ 'error' : ` Client Introuvable ! (${actor}) `})
 
       Livraison.create({ quantite, product : prods[0].id, client : clients[0].id }).fetch().then( async (liv) => {
         await Product.addToCollection(prods[0].id, 'livraisons').members([liv.id]);
         await Client.addToCollection(clients[0].id, 'livraisons').members([liv.id]);
-        sails.sockets.blast('newOp',{operation : 'Livraison',type : 'Client', actor,product})
+        sails.sockets.blast('newOp',{operation : 'Livraison',type : 'Client', actor,prods[0].title})
         return res.ok({ok : 'ok'});
       })
     },
@@ -67,13 +67,13 @@ module.exports = {
       let prods = await Product.find({ codeABarres : barCode});
       let fours = await Fournisseur.find({ nom : actor });
 
-      if( prods.length === 0 ) return res.ok({ 'error' : ` Produit Introuvable ! (${product}) `})
+      if( prods.length === 0 ) return res.ok({ 'error' : ` Produit Introuvable ! (${barCode}) `})
       if( fours.length === 0 ) return res.ok({ 'error' : ` Fournisseur Introuvable ! (${actor}) `})
 
       Reception.create({ quantite, product : prods[0].id, fournisseur : fours[0].id }).fetch().then( async (liv) => {
         await Product.addToCollection(prods[0].id, 'receptions').members([liv.id]);
         await Fournisseur.addToCollection(fours[0].id, 'receptions').members([liv.id]);
-        sails.sockets.blast('newOp',{operation : 'Réception',type: 'Fournisseur', actor,product})
+        sails.sockets.blast('newOp',{operation : 'Réception',type: 'Fournisseur', actor,prods[0].title})
         return res.ok({ok : 'ok'});
       })
     }
